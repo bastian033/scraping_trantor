b4fa0e1f15e83055df2accb9d9e8f2ef3f8aa906
Bastián Madrid <103474978+bastian033@users.noreply.github.com>
2025-04-17 09:53:29 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ import os
 
 app = Flask(__name__)
 
+# Configuración de MongoDB
 MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/")
 client = MongoClient(MONGO_URI)
-db = client['local'] 
-collection = db['DatosGob2025'] 
+db = client['local']  # Base de datos 'local'
 
 @app.route('/')
 def index():
@@ -19,14 +19,22 @@ def buscar_empresa():
     if not razon_social:
         return jsonify({"error": "Debe proporcionar una razon social para buscar"}), 400
 
-    resultados = list(collection.find({"Razon Social": {"$regex": razon_social, "$options": "i"}}))
-    for resultado in resultados:
-        resultado["_id"] = str(resultado["_id"]) # Convertir ObjectId a string para JSON
+    # Lista de colecciones a buscar
+    colecciones = [f"DatosGob{anio}" for anio in range(2013, 2026)]
+    resultados_totales = []
 
-    if not resultados:
+    # Iterar sobre cada colección y buscar
+    for coleccion_nombre in colecciones:
+        coleccion = db[coleccion_nombre]
+        resultados = list(coleccion.find({"Razon Social": {"$regex": razon_social, "$options": "i"}}))
+        for resultado in resultados:
+            resultado["_id"] = str(resultado["_id"])  # Convertir ObjectId a string para JSON
+        resultados_totales.extend(resultados)
+
+    if not resultados_totales:
         return jsonify({"message": "No se encontraron resultados para la razón social proporcionada"}), 404
 
-    return jsonify(resultados), 200
+    return jsonify(resultados_totales), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
